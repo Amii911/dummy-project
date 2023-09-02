66c90cf2f91e1e6bcace42d84400fe1b47454892
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-09-02 12:58:04 -0400

    updates

--- ami.py
+++ ami.py
@@ -1,2 +1,4 @@
 # I need to make daily commits. It starts today! I hope this works. 
 # need to add a point here becauase I need to see if this will add to green square!!!!
+
+# creatin g a ca cmt to push 9/2/23
