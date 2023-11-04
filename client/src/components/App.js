21a2762563490f39233b1ce8b2aaeecfde46c524
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-11-03 21:37:21 -0400

    adding to previous date

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 function App() {
   return <h1>Phase 4 Project Client
-    <p>Doin this!!!!git a</p>
-
-    <p>
-      git add . 
-      git commit --date='current date: YYYY-MM-DD' -m 'message'
-    </p>
+  
   </h1>;
 }
 
