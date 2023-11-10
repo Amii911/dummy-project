52b81e3f8446b79879f130b05db2386e6d1a6e5f
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-11-09 20:20:18 -0500

    adding to previous date

--- client/src/components/Mad.js
+++ client/src/components/Mad.js
@@ -3,6 +3,10 @@ import React from 'react'
 export const Mad = () => {
   return (
     <div>Mad
+        <h1>
+<p>we on our fucking bully! show activity for this month! </p>
+
+        </h1>
 
         <p>By fire by force this month will show activity!</p>
     </div>
