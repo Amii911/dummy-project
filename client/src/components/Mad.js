07fca0284d2855d79da6f1161a36524bd32b8b31
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-11-09 20:21:40 -0500

    adding to previous date

--- client/src/components/Mad.js
+++ client/src/components/Mad.js
@@ -6,6 +6,7 @@ export const Mad = () => {
         <h1>
 <p>we on our fucking bully! show activity for this month! 
     so this is going to be an offical diary entry!
+    Today is actually the 11/16/23.
 </p>
 
         </h1>
