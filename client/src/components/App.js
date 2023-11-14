9cc3a9b2afa48dc4a5a45f5571e21d55ea7c6969
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-11-13 20:51:43 -0500

    add all

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Phase 4 Project Client</h1>;
+  return <h1>Phase 4 Project Client
+    <p>Doin this!!!!</p>
+  </h1>;
 }
 
 export default App;
