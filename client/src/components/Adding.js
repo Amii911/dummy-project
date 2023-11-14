04cb142f9a8539de7e4a8d892de4bb85a5b6b226
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-11-13 20:17:35 -0500

    adding to previous date

--- client/src/components/Adding.js
+++ client/src/components/Adding.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Adding() {
   return (
-    <div>Adding to this damn field because I need to be active!</div>
+    <div>Adding to this damn field because I need to be active!
+
+        <p>because the way my actitivity needs to increase for the month!!!???</p>
+    </div>
   )
 }
 
